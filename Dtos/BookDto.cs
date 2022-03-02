059a507e8a513e4ed49997cd915f5bb582668cf4
Krystian Kluba <klubakrystian@gmail.com>
2022-03-02 20:23:32 +0100

    4-updated

--- Dtos/BookDto.cs
+++ Dtos/BookDto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using LibApp.Models;
 
 namespace LibApp.Dtos
 {
@@ -12,5 +13,11 @@ namespace LibApp.Dtos
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public Genre Genre { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int NumberInStock { get; set; }
+        public int NumberAvailable { get; set; }
     }
 }
